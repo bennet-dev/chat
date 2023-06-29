d12c724566a769c4156de83e83d5d77399d08b1f
Bennet Carpenter <bennetrcarpenter@gmail.com>
2023-06-29 06:35:36 -0700

    updated UI

--- src/features/chat-interface/index.tsx
+++ src/features/chat-interface/index.tsx
@@ -16,22 +16,22 @@ const ChatInterface = () => {
     };
 
     return (
-        <div class="flex flex-col">
-            <h1 class="text-6xl font-bold mb-4 ">Gandalf Chat</h1>
-            <div class="flex-grow overflow-auto mb-4 bg-white p-4 rounded-3xl shadow-lg bg-opacity-10 backdrop-blur-md border border-blue-300 border-opacity-60 max-h-96">
+        <div class="flex flex-col h-screen p-6">
+            <h1 class="text-6xl font-bold mb-4 ">AI Chat</h1>
+            <div class="flex-grow overflow-auto mb-4 bg-white p-4 rounded-3xl shadow-lg bg-opacity-10 backdrop-blur-md border border-blue-300 border-opacity-60">
                 <For each={messages()} >
                     {({ id, message, isHuman }) => (
-                        <div class={`flex min-w-full mb-2 animate-slide-in-${isHuman ? "right" : "left"}`}>
-                            <div class={`min-w-full text-left p-3 rounded-lg ${isHuman ? "bg-cyan-800 text-white" : "bg-gray-300 text-black"} bg-opacity-80`}>
+                        <div class={`flex  mb-2 animate-slide-in-${isHuman ? "right" : "left"}`}>
+                            <div class={`w-full py-2 px-4 rounded-lg ${isHuman ? "bg-blue-500 text-white" : "bg-gray-300 text-black"} bg-opacity-80 text-left`}>
                                 {message}
                             </div>
                         </div>
                     )}
                 </For>
             </div>
-            <div class="flex mt-4">
-                <input type="text" class="flex-grow mr-2 py-2 px-4 rounded-lg border border-blue-500 bg-white bg-opacity-10" placeholder="Type your message here" value={message()} onInput={(e) => setMessage(e.currentTarget.value)} disabled={loading()} />
-                <button class="py-2 px-4 bg-white  rounded-lg shadow-md bg-opacity-10  border border-blue-300" onClick={handleMessage} disabled={loading()}>
+            <div class="sticky bottom-0 flex mt-4 p-6 ">
+                <input type="text" class="flex-grow mr-2 py-2 px-4 rounded-lg border border-blue-500 bg-white bg-opacity-10  placeholder-white placeholder-opacity-40" placeholder="Type your message here" value={message()} onInput={(e) => setMessage(e.currentTarget.value)} disabled={loading()} />
+                <button class="py-2 px-4 bg-white text-blue-500 rounded-lg shadow-md bg-opacity-10 t border border-blue-300" onClick={handleMessage} disabled={loading()}>
                     {loading() ? "Loading..." : "Send"}
                 </button>
             </div>
--- src/services/chat/index.ts
+++ src/services/chat/index.ts
@@ -9,7 +9,7 @@ interface AIResponse {
 const chat = async (): Promise<AIResponse> => {
     const api_key = import.meta.env.VITE_OPENAI_API_KEY
 
-    const seed_message = new SystemChatMessage("You are Gandalf the Grey, attempting to get me to join you on your quest to rescue Frodo and Sam.");
+    const seed_message = new SystemChatMessage("You are a grizled detective interogating me for a crime.");
     const chat_log = messages().map(m => m.isHuman ? new HumanChatMessage(m.message) : new AIChatMessage(m.message))
 
     const chat = new ChatOpenAI({ openAIApiKey: api_key, temperature: 0 });
