097e5e044a58e690ca60dbe653aed20d4e50a91d
Bennet Carpenter <bennetrcarpenter@gmail.com>
2023-07-01 11:56:00 -0700

    deleted commented code.

--- src/features/chat-interface/index.tsx
+++ src/features/chat-interface/index.tsx
@@ -56,58 +56,6 @@ const ChatInterface = () => {
                 </button>
             </div>
         </div>
-        // <div class="flex flex-col h-screen p-6">
-        //     <h1 class="text-3xl font-bold mb-4 ">AI Chat</h1>
-        //     <div class="flex-grow overflow-auto mb-4 bg-white p-4 rounded-3xl shadow-lg bg-opacity-10 backdrop-blur-md border border-blue-300 border-opacity-60">
-        //         <For each={messages()} >
-        //             {({ text, _getType }) => (
-        //                 <div class={`flex  mb-2 animate-slide-in-${_getType() === "human" ? "right" : "left"}`}>
-        //                     <div class={`w-full py-2 px-4 rounded-lg  bg-opacity-80 text-left ${_getType() === "human" ? "bg-blue-500 text-white" : "bg-gray-300 text-black"}`}>
-        //                         {text}
-        //                     </div>
-        //                 </div>
-        //             )}
-        //         </For>
-        //         <Show when={sending.pending}>
-        //             <div>SENDING...</div>
-        //         </Show>
-        //         <Show when={sending.error}>
-        //             <div>
-        //                 ERROR: {sending.error.message}
-        //             </div>
-        //         </Show>
-        //     </div>
-        //     <div class="sticky bottom-0 flex mt-4 p-6 ">
-        //         <input
-        // type="text"
-        // placeholder="Type your message here"
-        // onKeyDown={(e) => e.key === "Enter" && handleSendMessage(message())}
-        // value={message()}
-        // onInput={(e) => setMessage(e.currentTarget.value)}
-        // disabled={sending.pending}
-        //             class="flex-grow mr-2 py-2 px-4 rounded-lg border border-blue-500 bg-white bg-opacity-10  placeholder-white placeholder-opacity-40"
-        //         />
-        //         <button
-        //             class="py-2 px-4 bg-white text-blue-500 rounded-lg shadow-md bg-opacity-10 t border border-blue-300"
-        //             onClick={() => handleSendMessage(message())}
-        //             disabled={sending.pending}
-        //         >
-        //             {sending.pending ? "Loading..." : "Send"}
-        //         </button>
-        //     </div>
-        // </div>
-        // <div>
-        //     <ChatInput type="text"
-        //         placeholder="Type your message here"
-        //         onKeyDown={(e) => e.key === "Enter" && handleSendMessage(message())}
-        //         value={message()}
-        //         onInput={(e) => setMessage(e.currentTarget.value)}
-        //         disabled={sending.pending}
-        //     />
-        //     <MessageBubble isHuman={false} >
-        //         Hello, How are you?
-        //     </MessageBubble>
-        // </div>
     );
 };
 
